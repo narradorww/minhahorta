3d84d9ddaaf6799a114faa7b87ac6fe3969e8df0
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2022-07-28 11:56:49 -0300

    Add a cors configuration to prevent cors errors

---
+++ src/main/java/br/com/balaiorural/minhahorta/Cors.java
@@ -0,0 +1,17 @@
+package br.com.balaiorural.minhahorta;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class Cors implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+    
+}
